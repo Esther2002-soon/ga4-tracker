20bfba1d99d5fdfd16d02073a6726c87faf1c72b
以瑭孫 <106828044+Esther2002-soon@users.noreply.github.com>
2025-05-21 12:44:49 +0800

    Update track.js 9

--- api/track.js
+++ api/track.js
@@ -51,5 +51,8 @@ export default async function handler(req, res) {
   `;
 
   res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
   res.status(200).send(html);
 }
